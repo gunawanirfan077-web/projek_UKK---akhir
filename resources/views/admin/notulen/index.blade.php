cc5725af82948710a439a4c65b0e5e8a7dd56285
gunawanirfan077 <gunawanirfan077@gmail.com>
2025-11-18 17:30:30 -0800

    memperbaiki kode

--- resources/views/admin/notulen/index.blade.php
+++ resources/views/admin/notulen/index.blade.php
@@ -4,10 +4,14 @@
 
 @section('content')
 <div class="container mt-4">
-
+    
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h3>📝 Daftar Notulen Kegiatan</h3>
-        <a href="{{ route('notulen.create') }}" class="btn btn-primary">+ Tambah Notulen</a>
+    </div>
+
+    <!-- Tombol Tambah Program -->
+    <div class="d-flex justify-content-end align-items-center mb-4">
+         <a href="{{ route('notulen.create') }}" class="btn btn-primary">+ Tambah Notulen</a>
     </div>
 
     @if(session('success'))
